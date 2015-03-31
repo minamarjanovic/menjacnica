fad67d102a04fb54287b01a784ad69072f57b0ab
minamarjanovic <mina.marjanovic5@gmail.com>
2015-04-01 00:17:40 +0200

    Napravljena je klasa koja implementira menjacnicaInterfejs

---
+++ src/menjacnica/Menjacnica.java
@@ -0,0 +1,27 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import menjacnicainterfejs.menjacnicaInterfejs;
+
+public class Menjacnica implements menjacnicaInterfejs {
+
+	@Override
+	public void dodajKurs(Kurs kurs) {
+		
+
+	}
+
+	@Override
+	public void obrisiKurs(Kurs kurs) {
+		
+
+	}
+
+	@Override
+	public double pronadjiIVrati(GregorianCalendar datum) {
+		
+		return 0;
+	}
+
+}
