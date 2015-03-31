3e0f7c986eebebb9b570fd2fb03cad1f10a40699
minamarjanovic <mina.marjanovic5@gmail.com>
2015-04-01 00:22:55 +0200

    Drugacije je implementirana metoda dodajKurs

--- src/menjacnica/Menjacnica.java
+++ src/menjacnica/Menjacnica.java
@@ -1,14 +1,27 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnicainterfejs.menjacnicaInterfejs;
 
 public class Menjacnica implements menjacnicaInterfejs {
-
+	
+	private LinkedList<Kurs> kursevi = new LinkedList<Kurs>();
+	
 	@Override
 	public void dodajKurs(Kurs kurs) {
+		if(kurs == null) {
+			throw new RuntimeException("Kurs ne sme biti null");
+		}
 		
+		for (int i = 0; i < kursevi.size(); i++) {
+			if(kursevi.get(i).equals(kurs)) {
+				System.out.println("Kurs vec postoji");
+				return;
+			}	
+		}
+		kursevi.add(kurs);
 
 	}
 
